77f811f56ba471fd1de49956f597d3191281b6f7
PixxySynchronous <theamazingpiyush@gmail.com>
2025-07-22 20:21:00 +0530

    Debugging

--- inotebook-backend/index.js
+++ inotebook-backend/index.js
@@ -1,16 +1,21 @@
 const connectToMongo = require('./db');
 const express = require('express'); 
-var cors = require('cors')
+const cors = require('cors');
+
 const app = express(); 
 const port = 5000;
 
+app.use(cors());
 app.use(express.json());
 
 connectToMongo(); 
 
-// Routes which are available
-app.use (cors())
+// Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
+
 app.listen(port, () => {
-  console.log(`iNotebbok-backend listening on port https://notemore-dashboard.onrender.com/:${port}`);
+  console.log(`Server listening locally on http://localhost:${port}`);
+});
+
+module.exports = app; // Optional but good for testing or Render
